7d9e172bc955b268a62597499ac227c18426e101
UjjwalAng <150407334+UjjwalAng@users.noreply.github.com>
2023-12-07 12:47:24 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, rem;
+    printf("Enter an integer that we will divide by 4: ");
+    scanf("%d", &num);
+    rem = num%4;
+
+    switch(rem){
+case 1:
+    printf("Remainder is 1");
+    break;
+case 2:
+    printf("Remainder is 2");
+    break;
+case 3:
+    printf("Remainder is 3");
+    break;
+default:
+    printf("There is no remainder");
+    }
+
+
+    return 0;
+}
