1fa9b91cba2a7be44aa33104affde4932233d8cb
UjjwalAng <150407334+UjjwalAng@users.noreply.github.com>
2023-12-22 20:59:54 +0530

    Add files via upload
    9. Array operations: Write a program to find average, max and min elements from an array of n long integers.

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int length;
+  printf("Enter length of array: ");
+  scanf("%d", &length);
+  int arr[length];
+
+  for (int i = 1; i < length+1; i++){
+    printf("Enter value %d: ", i);
+    scanf("%d", &arr[i-1]);
+  }
+
+  int max = arr[0];
+  for (int i = 1; i < length;i++){
+    if (arr[i]>max){
+      max = arr[i];
+    }
+  }
+
+  int min = arr[0];
+  for (int i = 1; i < length;i++){
+    if (arr[i]<min){
+      min = arr[i];
+    }
+  }
+
+  int sum = 0;
+  for (int i = 0; i <= length;i++){
+    sum += arr[i];
+  }
+  float avg = sum/length;
+
+  printf("The maximum element is: %d\nThe minimum element is: %d\nThe average of all the elements in the array is: %f", max, min, avg);
+
+  return 0;
+}
